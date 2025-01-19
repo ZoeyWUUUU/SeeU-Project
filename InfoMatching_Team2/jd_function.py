54b8a7c272f9499eea7b89fa5962406c40e99172
ty <146308838+tyliao925@users.noreply.github.com>
2025-01-19 09:32:07 +0800

    Add files via upload

--- InfoMatching_Team2/jd_function.py
+++ InfoMatching_Team2/jd_function.py
@@ -1,12 +1,14 @@
 import json
+
+import pandas as pd
 from openai import OpenAI
 with open('key.txt', 'r') as file:
     key = file.read()
 client = OpenAI(api_key=key)
 
-def extract_job_data(jd, client):
+def extract_job_data(jd):
     result = []
-    prompt = """extract detailed information from job description, including only 3 fields: Major, Tech skills and Experience domain. 
+    prompt = """extract detailed information from job description, including only the following fields: Company Industry Field(Search it), Major, Tech skills, graduation time(return not specified if not mentioned), minimum academic qualification and Experience domain. 
                 Always output plain JSON without any markdown or formatting. \n
                 Job description is:""" + jd
     try:
@@ -22,3 +24,8 @@ def extract_job_data(jd, client):
     except Exception as e:
         result.append({"error": f"Failed to process JD: {e}"})
     return result
+
+file_path = './CS.xlsx'
+df = pd.read_excel(file_path)
+jd1 = df['岗位要求'][20]
+result = extract_job_data(jd1)
